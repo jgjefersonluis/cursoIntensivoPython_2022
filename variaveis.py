6a75b5c98d3db2e5041dddba22a09bb9fc160153
jgjefersonluis <jgjefersonluis@gmail.com>
2022-11-20 21:13:48 -0300

    hello, world e variaveis

---
+++ variaveis.py
@@ -0,0 +1,17 @@
+nome = "John Louise"
+sobrenome = "Kenedy Orange"
+sexo = "Masculino"
+idade = 51
+signo = "Virginiano"
+data_nascimento = "11/04/1971"
+casado = "Casado com Dina de Borne Bistecas"
+filho = "Jasmin Bisteca Orange"
+
+
+print(nome, sobrenome)
+print(sexo)
+print(idade)
+print(signo)
+print(data_nascimento)
+print(casado)
+print(filho)
