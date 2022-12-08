3de8c01757587a3b44f3afd7340a4e957ee811e4
jgjefersonluis <jgjefersonluis@gmail.com>
2022-12-08 17:53:08 -0300

    tuplas

---
+++ aula06-listas/alterandoListas.py
@@ -0,0 +1,9 @@
+linguagens = ["Python", "C", "Java"]
+print(linguagens)
+linguagens.append("ada")
+print(linguagens)
+linguagens.append("PHP")
+print(linguagens)
+removido = linguagens.pop()
+print("Item removido: " + removido)
+print(linguagens)
