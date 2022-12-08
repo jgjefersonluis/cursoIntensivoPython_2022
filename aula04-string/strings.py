0b5c860f48794b1b49a054d75b4769d4ebbffda0
jgjefersonluis <jgjefersonluis@gmail.com>
2022-12-08 17:45:21 -0300

    string

---
+++ aula04-string/strings.py
@@ -0,0 +1,15 @@
+musica = "Garotos II - Leoni"
+music = "os outros"
+band = "kid abelha"
+
+print(musica)
+
+print("Seus olhos e seus olhares")
+print("Milhares de tentações")
+print("Meninas são tão mulheres")
+print("Seus truques e confusões")
+
+print(music.title())
+print(music.upper()) # todos maisculos
+print(music.lower()) # todos minusculos
+print(music.title() + " - " + band.title())
